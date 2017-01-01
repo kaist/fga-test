cb7817429ab82de189350f88b5272cbef5e5c941
kaist <igor.kaist@gmail.com>
2017-01-01 21:17:14 +0300

    hello world
    Hello world

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+
+
+var main = new UI.Card({
+
+  banner: 'images/app_icon.png',
+  body: 'Hello world'
+
+});
+
+main.show();
+
+
